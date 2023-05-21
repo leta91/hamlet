7beafc04f8b3f5b49db9fede1ec0892057e905b4
Letizia DI MAIO <109817389+leta91@users.noreply.github.com>
2023-05-21 11:23:10 +0100

    pasted <footer> section up to </div> from front-page.php

--- footer.php
+++ footer.php
@@ -1,4 +1,12 @@
-<?php
+<footer class="footer text-center py-2 theme-bg-dark">
+		   
+           <p class="copyright"><a href="https://letiziadimaio.netlify.app/">Letizia DI MAIO</a></p>
+          
+       </footer>
+   
+   </div>
+   
+   <?php
 	wp_footer();  
 	?>
 
