f2a9a55e638a3449c0cf296d9391e3b0abe6b5dd
Letizia DI MAIO <109817389+leta91@users.noreply.github.com>
2023-05-21 17:15:16 +0100

    produced site logo(correct size) and primary menu(<ul> style applied)

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Blog Site Template">
     <meta name="author" content="https://letiziadimaio.netlify.app/">    
-    <link rel="shortcut icon" href="images/logo.png"> 
+    <link rel="shortcut icon" href="wp-content/themes/hamlet/assets/images/logo.png"> 
     
     <?php
 	wp_head();  // Prints scripts or data in the <head>
@@ -27,25 +27,36 @@
 			</button>
 
 			<div id="navigation" class="collapse navbar-collapse flex-column" >
-				<img class="mb-3 mx-auto logo" src="images/logo.png" alt="logo" >			
+
+
+            <!-- it will output the site logo -->
+
+             <?php
+               if (function_exists('the_custom_logo')) {
+                
+                $custom_logo_id = get_theme_mod('custom_logo');
+                $logo = wp_get_attachment_image_src($custom_logo_id);
+               }
+            ?>
+			
+              <img class="mb-3 mx-auto logo"
+                src="<?php echo $logo[0] ?>" alt="logo" >
+            
+                <!-- it will create dynamic WP menus with styling of its elements copied from
+                the HTML template -->
+
+                <?php
+                wp_nav_menu(
+                 array(
+                  'menu'=> 'primary',
+                  'container'=> '',
+                  'theme_location'=>'primary',
+                  'items_wrap'=>'<ul div id= "" class="navbar-nav flex-column text-sm-center text-md-left">
+                  %3$s</ul>'
+        )
+    );
+    ?>
 				
-				<ul class="navbar-nav flex-column text-sm-center text-md-left">
-					<li class="nav-item active">
-					    <a class="nav-link" href="index.html"><i class="fas fa-home fa-fw mr-2"></i>Blog Home <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item">
-					    <a class="nav-link" href="post.html"><i class="fas fa-file-alt fa-fw mr-2"></i>Blog Post</a>
-					</li>
-					<li class="nav-item">
-					    <a class="nav-link" href="page.html"><i class="fas fa-file-image fa-fw mr-2"></i>Blog Page</a>
-					</li>
-					<li class="nav-item">
-					    <a class="nav-link" href="archive.html"><i class="fas fa-archive fa-fw mr-2"></i>Blog Archive</a>
-					</li>
-					<li class="nav-item">
-					    <a class="nav-link btn btn-primary" href="contact.html"><i class="fas fa-envelope fa-fw mr-2"></i>Contact Us</a>
-					</li>
-				</ul>
 				<hr>
 				<ul class="social-list list-inline py-3 mx-auto">
 					<li class="list-inline-item"><a href="#"><i class="fab fa-twitter fa-fw"></i></a></li>
@@ -59,8 +70,10 @@
 		</nav>
     </header>
 
+    <!-- the_title(); in PHP outputs the page title heading dynamically -->
+
     <div class="main-wrapper">
 	    <header class="page-title theme-bg-light text-center gradient py-5">
-			<h1 class="heading">Blog Home Page Heading</h1>
+			<h1 class="heading"><?php the_title(); ?></h1>  
 		</header>
 		
