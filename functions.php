b4510c7c357314a412144a638bb457bc3334c62a
Letizia DI MAIO <109817389+leta91@users.noreply.github.com>
2023-06-26 17:05:25 +0100

    registered footer widget

--- functions.php
+++ functions.php
@@ -86,15 +86,25 @@ function hamlet_widget_areas() {
         array(
             'before_title' => '',
             'after_title' => '',
-            'before_widget' => '',
-            'after_widget' => '',
-        ),
-        array(
+            'before_widget' => '<ul class="social-list list-inline py-3 mx-auto">',
+            'after_widget' => '</ul>',
             'name' => 'Sidebar Area',
             'id' => 'sidebar-1',
             'description' => 'Sidebar Widget Area'
         )
         );
+
+        register_sidebar(
+            array(
+                'before_title' => '',
+                'after_title' => '',
+                'before_widget' => '<ul class="social-list list-inline py-3 mx-auto">',
+                'after_widget' => '</ul>',
+                'name' => 'Footer Area',
+                'id' => 'footer-1',
+                'description' => 'Footer Widget Area'
+            )
+            );
 }
 add_action( 'widgets_init', 'hamlet_widget_areas' );
 
