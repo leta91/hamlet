88fae53775e18a31c4be824400800373d0b2e37b
Letizia DI MAIO <109817389+leta91@users.noreply.github.com>
2023-06-26 18:30:43 +0100

    added 404 page with search box

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+  get_header();     // loads header template
+?>
+    
+    <article class="content px-3 py-5 p-md-5">
+
+    <h1>Page not Found</h1>
+      <?php 
+        get_search_form();  // loads page not found and search box
+       ?>
+	    
+	    </article>
+	    
+
+	<?php
+	  get_footer();    // loads footer template
+	?>
