ddbcf61ed0857c65dcbe478031cacfb96ce0d9cf
Letizia DI MAIO <109817389+leta91@users.noreply.github.com>
2023-07-01 15:18:20 +0100

    link to site title changed to WP homepage

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
 <!--get_bloginfo('name'); loads the site name dynamically and allows to be edited -->
     
     <header class="header text-center">	    
-	    <a class="site-title pt-lg-4 mb-0" href="index.html">
+	    <a class="site-title pt-lg-4 mb-0" href="http://hamlet.local/">
         <?php echo get_bloginfo('name'); ?></a>             
         
 	    <nav class="navbar navbar-expand-lg navbar-dark" >
