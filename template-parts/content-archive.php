7adeb90166779e0fd35d94dd81277b273fbaf1e9
Letizia DI MAIO <109817389+leta91@users.noreply.github.com>
2023-07-01 15:17:32 +0100

    removed comments number in the archive page

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -8,8 +8,8 @@
 						    <h3 class="title mb-1"><a href="<?php the_permalink(); ?>"> <!-- to make post titles clickable -->
                             <?php the_title(); ?></a></h3>
 						    <div class="meta mb-1"><span class="date"><?php the_date(); ?></span>
-                            <span class="comment"><a href="#"><?php comments_number(); ?></a></span></div>
-						    <div class="intro"> <?php the_excerpt(); ?></div>
+                            <span class="comment"><a href="#">Comments</a></span></div>
+						    
 						    <a class="more-link" href="<?php the_permalink(); ?>">Read more &rarr;</a>
 					    </div><!--//media-body-->
 				    </div><!--//media-->
